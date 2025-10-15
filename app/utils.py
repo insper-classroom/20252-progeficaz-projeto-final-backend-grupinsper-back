bcee593d9abea63b4b910917feb918c2e14f9f7d
mthperera <146563896+mthperera@users.noreply.github.com>
2025-10-14 22:24:19 -0300

    feat: Adicionando classificador de BancoCandidato por meio de llm via b64

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,4 @@
+import base64
 from datetime import date
 from enum import Enum
 from io import BufferedReader
@@ -5,9 +6,10 @@ import os
 from operator import itemgetter
 import requests
 from requests.models import Request
-from typing import List
+from typing import List, Union
 
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from pydantic import Field, BaseModel
@@ -57,11 +59,12 @@ class Banco(Enum):
     MERCANTIL_DO_BRASIL     = "MERCANTIL_DO_BRASIL"
     PAN                     = "PAN"
     BMG                     = "BMG"
-    OUTROS                  = "OUTROS"
+    OUTRO                   = "OUTRO"
+    NAO_IDENTIFICADO        = "NAO_IDENTIFICADO"
 
 
 class BancoCandidato(BaseModel):
-    banco: Banco = Field(..., description="O banco que o extrato pertence. Caso não saiba, coloque outros.")
+    banco: Banco = Field(..., description="O banco que o extrato pertence. Caso não saiba, coloque NAO_IDENTIFICADO.")
     score: float = Field(..., description="Pontuação entre 0.0 e 1.0. Apenas deixe acima de 0.8 se tiver **certeza absoluta** há informações para concluir que é o banco correto.")
 
 
@@ -109,6 +112,48 @@ def get_extrato_parser(id: str, type_result: str) -> Request:
         raise ValueError("A variável de ambiente LLAMA_CLOUD_API_KEY não está definida.")
 
     url = f"https://api.cloud.llamaindex.ai/api/v1/parsing/job/{id}/result/{type_result}"
+
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+
+    response = requests.get(url, headers=headers)
+    
+    return response
+
+
+def get_extrato_images_names(id: str) -> Union[List[str], None]:
+
+    API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
+    if not API_KEY:
+        raise ValueError("A variável de ambiente LLAMA_CLOUD_API_KEY não está definida.")
+
+    url = f"https://api.cloud.llamaindex.ai/api/v1/parsing/job/{id}/result/json"
+
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+
+    response = requests.get(url, headers=headers)
+    
+    images_names = []
+    if response.status_code == 200:
+        pages = response.json()["pages"]
+        for page in pages:
+            for image in page["images"]:
+                name = image["name"]
+                if not name in images_names and len(images_names) < 3:
+                    images_names.append(name)
+        return images_names
+
+    return None
+
+
+def get_extrato_images(id: str, image_name: List[str]) -> Request:
+    # Para conseguir o binário, dê um .content na Response
+
+    API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
+    if not API_KEY:
+        raise ValueError("A variável de ambiente LLAMA_CLOUD_API_KEY não está definida.")
+
+    url = f"https://api.cloud.llamaindex.ai/api/v1/parsing/job/{id}/result/image/{image_name}"
+
     headers = {"Authorization": f"Bearer {API_KEY}"}
 
     response = requests.get(url, headers=headers)
@@ -148,7 +193,9 @@ def get_extrato_estruturado(extrato_string: str) -> Extrato:
     - PESSOA_FISICA: transferência para CPF ou nome próprio de pessoa  
     - OUTROS: caso não se enquadre em nenhum acima
 
-    5. **Mapear `banco: BancoCandidato`** da transferência.
+    5. **Mapear `banco: BancoCandidato`** da transferência. 
+    Caso não exista informações para dizer qual o banco, identifique como "NAO_IDENTIFICADO" e retorne um baixo score de certeza.
+    Caso não seja nenhuma das opções de banco, mas exista informação para concluir que é um banco dos que não está nas opções, coloque "OUTRO".
     
     6. **Mapear `data: date`** do extrato. Você colocar a data do primeiro dia do mês que o extrato se refere. Temos várias datas diferentes, porém todas com o mesmo mês. Identique o mês.
     """
@@ -166,5 +213,43 @@ def get_extrato_estruturado(extrato_string: str) -> Extrato:
     )
 
     response = chain.invoke({"extrato": extrato_string})
+
+    return response
+
+
+def get_banco_candidato(image_in_binary: bytes, file_format: str) -> BancoCandidato:
+
+    image_b64 = base64.b64encode(image_in_binary).decode("utf-8")
+
+    model = ChatOpenAI(model="gpt-4o")
+    structured_model = model.with_structured_output(BancoCandidato)
+
+    system_message = """
+    Você é um classificador de banco. Você recebe a imagem de um banco em base64 e deve retornar um objeto do tipo BancoCandidato.
+
+    Caso a imagem não seja de uma logo de banco, identifique como "NAO_IDENTIFICADO" e retorne um baixo score de certeza.
+    Caso não seja nenhuma das opções de banco, mas exista informação para concluir que é um banco dos que não está nas opções, coloque "OUTRO".
+    """
+
+    human_message = {
+            "type": "image",
+            "source_type": "base64",
+            "data": image_b64,
+            "mime_type": f"image/{file_format}",
+        },
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content=system_message),
+            HumanMessage(content=human_message)
+        ]
+    )
+
+    chain = (
+        prompt
+        | structured_model
+    )
+
+    response = chain.invoke({})
 
     return response
